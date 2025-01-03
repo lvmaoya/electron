64f051054880cc155734670a9620893b3219cef8
sunjianxiang <1504734652@qq.com>
2025-01-03 09:22:56 +0800

    系统托盘

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,Tray,Menu } = require('electron')
 const NODE_ENV = process.env.env
 const path = require('path')
 const createWindow = () => {
@@ -13,14 +13,33 @@ const createWindow = () => {
 
   // 加载 index.html
   if (NODE_ENV === "development") {
-    // mainWindow.loadURL('http://localhost:3000/')
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
+    mainWindow.loadURL('http://localhost:3000/')
+    // mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
   }else{
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
   }
   if (NODE_ENV === "development") {
     mainWindow.webContents.openDevTools()
   }
+
+  //创建系统托盘
+  const tray = new Tray(path.join(__dirname, '../assets/todo.png'))
+
+  // 托盘右键菜单
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'About', click: () => { console.log('About clicked') } },
+    { type: 'separator' },
+    { label: 'Quit', click: () => { app.quit() } }
+  ])
+  tray.setToolTip('Todo App');
+  tray.setContextMenu(contextMenu)
+  tray.on('click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+  });
+  // 接收来自 renderer进程的消息
+  mainWindow.webContents.on('message', (event, message) => {
+    console.log(`Received message: ${message}`)
+  })
 
 }
 
